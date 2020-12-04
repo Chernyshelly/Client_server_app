4ec801634b2394f661b5c82339cb9fee4e62c538
Chernyshelly <andreycherni@gmail.com>
2020-12-04 18:37:34 +0300

    Final edit before launch

--- WebAAA/WebAAA/Controllers/HomeController.cs
+++ WebAAA/WebAAA/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebAAA.Models;
 
@@ -15,6 +16,21 @@ namespace WebAAA.Controllers
         public HomeController(ApplicationContext context)
         {
             db = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
     /*public class HomeController : Controller
